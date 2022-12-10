a7b34581abcb4ec029d1e7d11513e372443960f3
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-12-10 15:12:20 +0600

    Basic Component Setup Done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -8,12 +7,6 @@ import '@fontsource/roboto/700.css';
 function App() {
   return (
     <div>
-      <button className="btn btn-active">Button</button>
-      <button className="btn btn-active btn-primary">Button</button>
-      <button className="btn btn-active btn-secondary">Button</button>
-      <button className="btn btn-active btn-accent">Button</button>
-      <button className="btn btn-active btn-ghost">Button</button>
-      <button className="btn btn-active btn-link">Button</button>
     </div>
   );
 }
