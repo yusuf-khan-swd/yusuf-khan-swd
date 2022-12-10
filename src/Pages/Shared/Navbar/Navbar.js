075b6b0d1aa8cc7a32994e191a2f8865ce70765c
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-12-10 18:24:28 +0600

    added navbar bg and sticky to top

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -8,9 +8,9 @@ const Navbar = () => {
   </>
 
   return (
-    <div>
+    <div className='bg-gray-600 sticky top-0 z-50'>
       <div className='container mx-auto'>
-        <div className="navbar bg-base-100">
+        <div className="navbar p-0">
           <div className="navbar-start">
             <div className="dropdown">
               <label tabIndex={0} className="btn btn-ghost lg:hidden">
