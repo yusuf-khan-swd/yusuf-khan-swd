e72132db342e02743902b1d8e7989242c681660f
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-12-10 22:47:40 +0600

    fix some issue with banner and card

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -17,13 +17,13 @@ const Banner = () => {
             }}></Typewriter>
             <p className="text-sm">A self-motivated and enthusiastic web developer with a deep interest in JavaScript and other programming languages. Seeking a Web Developer position in a company where I can use my skills and develop my skills.</p>
             <div className='mt-6'>
-              <a className="bg-gray-600 btn btn-ghost w-full md:w-52 mb-3 md:mr-2" href='https://drive.google.com/file/d/1qleVLJGI5jU6fmDItFsJISNvf6v3UZCs/view' target={`_blank`}>Download My Resume</a>
-              <a href='https://form.jotform.com/223433296548058' target={`_blank`} className='btn w-full md:w-52'>Contact with Me</a>
+              <a className="bg-gray-600 btn btn-ghost w-full lg:w-52 mb-3 md:mr-2" href='https://drive.google.com/file/d/1qleVLJGI5jU6fmDItFsJISNvf6v3UZCs/view' target={`_blank`}>Download My Resume</a>
+              <a href='https://form.jotform.com/223433296548058' target={`_blank`} className='btn w-full lg:w-52'>Contact with Me</a>
             </div>
           </div>
         </div>
-        <div className='rounded-lg'>
-          <figure><img className='h-72 mx-auto rounded-full' src={myPic} alt="Yusuf" /></figure>
+        <div className='rounded-lg flex justify-center items-center lg:items-start'>
+          <figure><img className='h-72 rounded-[100%]' src={myPic} alt="Yusuf" /></figure>
         </div>
       </div>
     </div>
