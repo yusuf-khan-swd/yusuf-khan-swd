bc77517b11a87e1203ffe7ba426b33c445476502
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-12-10 19:30:09 +0600

    custom contact form

--- src/Pages/Home/Contact/Contact.js
+++ src/Pages/Home/Contact/Contact.js
@@ -10,14 +10,18 @@ const Contact = () => {
 
   return (
     <div>
-      <h2>Contact</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <h2 className='text-3xl font-bold text-center mt-3'>Contact Me</h2>
+      <p className='text-center text-sm'>Fell free to contact me</p>
+      <form className='card max-w-sm mx-auto' onSubmit={handleSubmit(onSubmit)}>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Your Name</span>
           </label>
-          <input {...register("name", { required: "Please enter your name" })} type="text" placeholder="password" className="input input-bordered" />
+          <input {...register("name", { required: "Please enter your name" })} type="text" className="input input-bordered" />
           <p className="text-red-500">{errors.name?.message}</p>
+        </div>
+        <div className='form-control mt-5'>
+          <button className='btn w-full' type="submit">Submit</button>
         </div>
       </form>
     </div>
