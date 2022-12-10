ad2c611acd841608c9da1265e07e7dc725c64b90
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-12-10 23:02:01 +0600

    added error element

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
 import Contact from "../../Pages/Home/Contact/Contact";
 import Home from "../../Pages/Home/Home/Home";
+import DisplayError from "../../Pages/Shared/DisplayError/DisplayError";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    errorElement: <DisplayError></DisplayError>,
     children: [
       {
         path: "/",
