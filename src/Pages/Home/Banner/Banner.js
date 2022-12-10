727c03664defb3aaa46aebf26d8d5685b877f1e3
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-12-10 17:13:15 +0600

    Banner Section done

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -4,20 +4,22 @@ import myPic from '../../../assets/my-pic.jpg';
 
 const Banner = () => {
   return (
-    <div className='mt-8'>
+    <div className='mt-8 mb-16'>
       <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
-        <div className='card'>
+        <div className='card order-last md:order-first'>
           <div className='card-body'>
-            <h2>Hello I'm, Yusuf</h2>
-            <p>A MERN Stack Developer</p>
-            <div>
-              <a className="bg-gray-600 btn btn-ghost w-full md:w-44 mb-2 md:mr-2" href='https://drive.google.com/file/d/1qleVLJGI5jU6fmDItFsJISNvf6v3UZCs/view' target={`_blank`}>Download Resume</a>
-              <Link className='btn w-full md:w-44'>Contact Me</Link>
+            <p>Hello I'm,</p>
+            <h2 className='card-title text-6xl'>Yusuf</h2>
+            <p className='text-lg'>MERN Stack Developer</p>
+            <p className="text-sm">A self-motivated and enthusiastic web developer with a deep interest in JavaScript and other programming languages. Seeking a Web Developer position in a company where I can use my skills and develop my skills.</p>
+            <div className='mt-5'>
+              <a className="bg-gray-600 btn btn-ghost w-full md:w-52 mb-2 md:mr-2" href='https://drive.google.com/file/d/1qleVLJGI5jU6fmDItFsJISNvf6v3UZCs/view' target={`_blank`}>Download My Resume</a>
+              <Link to="/contact" className='btn w-full md:w-52'>Contact with Me</Link>
             </div>
           </div>
         </div>
-        <div>
-          <figure><img className='h-72 mx-auto md:ml-auto rounded-3xl' src={myPic} alt="Yusuf" /></figure>
+        <div className='rounded-lg'>
+          <figure><img className='h-72 mx-auto rounded-full' src={myPic} alt="Yusuf" /></figure>
         </div>
       </div>
     </div>
