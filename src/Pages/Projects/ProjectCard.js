a5ebcbecbd265abf3dd7a220b3f7d0a8889ce68f
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-12-11 14:29:19 +0600

    make fake data json file for project information

--- src/Pages/Projects/ProjectCard.js
+++ src/Pages/Projects/ProjectCard.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { PhotoView } from 'react-photo-view';
+import { Link } from 'react-router-dom';
 
 const ProjectCard = ({ project }) => {
   const { name, img, live, description } = project;
+  const projectDescription = description.length > 3 ? description.slice(0, 3) : description;
+
   return (
     <div>
       <div className="card card-compact shadow-xl border border-slate-600 h-full m-3 lg:m-0">
@@ -13,8 +16,13 @@ const ProjectCard = ({ project }) => {
         </figure>
         <div className="card-body">
           <h2 className="card-title">{name}</h2>
-          <p className='text-lg'>{description.length > 3 ? description.slice(0, 3) : description}</p>
+          <ul className='text-lg list-disc ml-4'>
+            {
+              projectDescription.map((description, index) => <li className='mb-1' key={index}>{description}</li>)
+            }
+          </ul>
           <div className="card-actions justify-end">
+            <Link className='btn' to={`/project/details/${project._id}`}>Details</Link>
             <a className="btn" href={live} target={`_blank`}>Live Site</a>
           </div>
         </div>
--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -1,46 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { PhotoProvider } from 'react-photo-view';
 import ProjectCard from './ProjectCard';
 import 'react-photo-view/dist/react-photo-view.css';
 
 const Projects = () => {
-  const projects = [
-    {
-      _id: 1,
-      name: "Thrift Store",
-      img: "https://i.ibb.co/YXr3Ty1/thrift-store.png",
-      live: "https://thrift-store-88291.web.app/",
-      description: [
-        "Create Admin, Seller, and Buyer private routes to give access only to the same user type.",
-        "Buyers can buy and pay with the stripe payment method system.",
-        "JWT token to verify a user is authorized. Using middleware check a user Admin or Seller.",
-        "User can create seller or buyer account"
-      ],
-    },
-    {
-      _id: 2,
-      name: "Legal Network",
-      img: "https://i.ibb.co/R2ZC89S/legal-network.png",
-      live: "https://legal-network-a9c8a.web.app/",
-      description: [
-        "This project has private routes to block an unauthenticated user.",
-        "Used JWT token to verify a user is authorized to access this route.",
-        "After the user verifies Users can add a service, add a review and delete or modify the review."
-      ],
-    },
-    {
-      _id: 3,
-      name: "Invest In Knowledge",
-      img: "https://i.ibb.co/tJ5Rs48/invest-in-knowledge.png",
-      live: "https://invest-in-knowledge.web.app/",
-      description: [
-        "Users can log in or register with email and password or with Google And Github.",
-        "Using routing shows users different components.",
-        "Private routing feature to block unauthorized users.",
-        "If a user is logged in then the user can access the private route."
-      ],
-    }
-  ];
+  const [projects, setProjects] = useState([]);
+  const [isDataLoading, setIsDataLoading] = useState(false);
+
+  useEffect(() => {
+    setIsDataLoading(true);
+    fetch("fakeProjectData.json")
+      .then(res => res.json())
+      .then(data => {
+        setProjects(data);
+        setIsDataLoading(false);
+      })
+  }, [isDataLoading]);
+
+  if (isDataLoading) {
+    return <h2 className='text-3xl font-bold'>Loading....</h2>
+  }
+
   return (
     <div className='container mx-auto mt-8'>
       <h2 className="text-4xl font-bold ml-3 text-center mb-8">Some of My Projects</h2>
