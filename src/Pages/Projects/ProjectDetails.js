d77371a1f9207432d60ec5b66874f34a1ab622b7
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-12-11 22:20:43 +0600

    added max witdth

--- src/Pages/Projects/ProjectDetails.js
+++ src/Pages/Projects/ProjectDetails.js
@@ -27,26 +27,32 @@ const ProjectDetails = () => {
   const projectDescription = description;
 
   return (
-    <div>
-      <div className="card shadow-xl border border-slate-600 h-full m-3 lg:m-0">
-        <figure className='w-full'>
-          <PhotoView src={img}>
-            <img className='w-full h-full' src={img} alt={name} />
-          </PhotoView>
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">{name}</h2>
-          <ul className='text-lg list-disc ml-4 grid'>
-            {
-              projectDescription.map((description, index) => <li className='mb-1' key={index}>{description}</li>)
-            }
-          </ul>
-          <div className="card-actions justify-end">
-            <a className="btn" href={live} target={`_blank`}>Live Site</a>
+    <div className='container mx-auto my-16'>
+      <div className='max-w-6xl mx-auto'>
+        <div className="card shadow-xl border border-slate-600 h-full m-3 lg:m-0">
+          <figure className='w-full'>
+            <PhotoView src={img}>
+              <img className='w-full h-full' src={img} alt={name} />
+            </PhotoView>
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{name}</h2>
+            <ul className='text-lg list-disc ml-4 grid'>
+              {
+                projectDescription.map((description, index) => <li className='mb-1' key={index}>{description}</li>)
+              }
+            </ul>
+            <div className="card-actions justify-end">
+              <div>
+                <a className='btn' href="https://github.com/yusuf-khan-swd">Github Link</a>
+              </div>
+              <div>
+                <a className="btn" href={live} target={`_blank`}>Live Site</a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-
     </div>
   );
 };
