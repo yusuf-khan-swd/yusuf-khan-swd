9af6a43b684334433ce4425fe5fa4ab2076c7f0e
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-12-11 12:55:15 +0600

    back to drive link and format with prettier

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,31 +1,61 @@
-import React from 'react';
-import myPic from '../../../assets/my-pic.jpg';
-import Typewriter from 'typewriter-effect';
-import { FaEnvelope, FaFacebookSquare, FaGithub, FaLinkedin } from 'react-icons/fa';
+import React from "react";
+import myPic from "../../../assets/my-pic.jpg";
+import Typewriter from "typewriter-effect";
+import {
+  FaEnvelope,
+  FaFacebookSquare,
+  FaGithub,
+  FaLinkedin,
+} from "react-icons/fa";
 
 const Banner = () => {
   return (
-    <div className='mt-8 mb-16'>
-      <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
-        <div className='card order-last md:order-first'>
-          <div className='card-body'>
-            <p className='text-lg'>Hello I'm,</p>
-            <h2 className='card-title text-6xl'>Yusuf</h2>
-            <div className='font-bold text-lg'>
-              <Typewriter options={{
-                strings: ['A MERN Stack Developer', 'A JavaScript Developer', 'A React Developer'],
-                autoStart: true,
-                loop: true,
-              }}></Typewriter>
+    <div className="mt-8 mb-16">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div className="card order-last md:order-first">
+          <div className="card-body">
+            <p className="text-lg">Hello I'm,</p>
+            <h2 className="card-title text-6xl">Yusuf</h2>
+            <div className="font-bold text-lg">
+              <Typewriter
+                options={{
+                  strings: [
+                    "A MERN Stack Developer",
+                    "A JavaScript Developer",
+                    "A React Developer",
+                  ],
+                  autoStart: true,
+                  loop: true,
+                }}
+              ></Typewriter>
             </div>
-            <p className="text-lg">A self-motivated and enthusiastic web developer with a deep interest in JavaScript and other programming languages. Seeking a Web Developer position in a company where I can use my skills and develop my skills.</p>
-            <div className='mt-6'>
-              <a className="dark:bg-gray-600 bg-slate-400 btn btn-ghost w-full lg:w-52 mb-3 md:mr-2" href='https://drive.google.com/uc?export=download&id=1qleVLJGI5jU6fmDItFsJISNvf6v3UZCs'>Download My Resume</a>
-              <a href='https://form.jotform.com/223433296548058' target={`_blank`} className='btn w-full lg:w-52'>Contact with Me</a>
+            <p className="text-lg">
+              A self-motivated and enthusiastic web developer with a deep
+              interest in JavaScript and other programming languages. Seeking a
+              Web Developer position in a company where I can use my skills and
+              develop my skills.
+            </p>
+            <div className="mt-6">
+              <a
+                className="dark:bg-gray-600 bg-slate-400 btn btn-ghost w-full lg:w-52 mb-3 md:mr-2"
+                href="https://drive.google.com/file/d/1qleVLJGI5jU6fmDItFsJISNvf6v3UZCs/view"
+                target={`_blank`}
+              >
+                Download My Resume
+              </a>
+              <a
+                href="https://form.jotform.com/223433296548058"
+                target={`_blank`}
+                className="btn w-full lg:w-52"
+              >
+                Contact with Me
+              </a>
             </div>
             <div>
               <button>
-                <a href="https://wwww.facebook.com"><FaFacebookSquare className='dark:text-slate-400 dark:hover:text-slate-500 text-blue-700 hover:text-blue-600 text-5xl'></FaFacebookSquare></a>
+                <a href="https://wwww.facebook.com">
+                  <FaFacebookSquare className="dark:text-slate-400 dark:hover:text-slate-500 text-blue-700 hover:text-blue-600 text-5xl"></FaFacebookSquare>
+                </a>
               </button>
               <button>
                 <FaGithub></FaGithub>
@@ -39,8 +69,10 @@ const Banner = () => {
             </div>
           </div>
         </div>
-        <div className='rounded-lg flex justify-center items-center lg:items-start'>
-          <figure><img className='h-72 rounded-[100%]' src={myPic} alt="Yusuf" /></figure>
+        <div className="rounded-lg flex justify-center items-center lg:items-start">
+          <figure>
+            <img className="h-72 rounded-[100%]" src={myPic} alt="Yusuf" />
+          </figure>
         </div>
       </div>
     </div>
--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,35 +1,75 @@
-import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  const menuItems = <>
-    <li><NavLink className="m-1" to="/home">Home</NavLink></li>
-    <li><a className="m-1" href="https://form.jotform.com/223433296548058" target={`_blank`}>Contact</a></li>
-    <li><a className="m-1" href="#projects">Project</a></li>
-  </>
+  const menuItems = (
+    <>
+      <li>
+        <NavLink className="m-1" to="/home">
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <a
+          className="m-1"
+          href="https://form.jotform.com/223433296548058"
+          target={`_blank`}
+        >
+          Contact
+        </a>
+      </li>
+      <li>
+        <a className="m-1" href="#projects">
+          Project
+        </a>
+      </li>
+    </>
+  );
 
   return (
-    <div className='dark:bg-gray-600 bg-slate-400 sticky top-0 z-50'>
-      <div className='container mx-auto'>
+    <div className="dark:bg-gray-600 bg-slate-400 sticky top-0 z-50">
+      <div className="container mx-auto">
         <div className="navbar p-0">
           <div className="navbar-start">
             <div className="dropdown">
               <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
               </label>
-              <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+              <ul
+                tabIndex={0}
+                className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+              >
                 {menuItems}
               </ul>
             </div>
-            <Link className="btn btn-ghost normal-case text-xl" to="/">Yusuf Khan</Link>
+            <Link className="btn btn-ghost normal-case text-xl" to="/">
+              Yusuf Khan
+            </Link>
           </div>
           <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal px-1">
-              {menuItems}
-            </ul>
+            <ul className="menu menu-horizontal px-1">{menuItems}</ul>
           </div>
           <div className="navbar-end">
-            <a className="btn mr-2 sm:mr-0 btn-outline" href='https://drive.google.com/uc?export=download&id=1qleVLJGI5jU6fmDItFsJISNvf6v3UZCs'>Resume</a>
+            <a
+              className="btn mr-2 sm:mr-0 btn-outline"
+              href="https://drive.google.com/file/d/1qleVLJGI5jU6fmDItFsJISNvf6v3UZCs/view"
+              target={`_blank`}
+            >
+              Resume
+            </a>
           </div>
         </div>
       </div>
