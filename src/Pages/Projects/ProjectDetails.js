a7868ed0b972683730c88b91cd4a72003f4d0f7b
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-12-11 19:47:40 +0600

    json added

--- src/Pages/Projects/ProjectDetails.js
+++ src/Pages/Projects/ProjectDetails.js
@@ -2,18 +2,25 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import Loading from '../Shared/Loading/Loading';
 
 const ProjectDetails = () => {
   const { id } = useParams();
 
   const { data: projects, isLoading } = useQuery({
-    queryKey: ["project"],
+    queryKey: ["projects"],
     queryFn: async () => {
-      const res = await axios.get("fakeProjectData");
-      const data = await res.data;
+      const res = await fetch("fakeProjectData.json");
+      const data = await res.json();
       return data;
     }
   });
+
+  if (isLoading) {
+    return <Loading></Loading>
+  }
+
+  console.log(projects);
 
   return (
     <div>
