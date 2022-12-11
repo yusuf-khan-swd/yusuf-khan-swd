239d8b09093d5baeacda5dce3ee4350414dc259c
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-12-11 21:44:37 +0600

    added blog text

--- src/Pages/Others/Blog/Blog.js
+++ src/Pages/Others/Blog/Blog.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Blog = () => {
   return (
     <div>
-      <h2 className="text-3xl font-bold">Coming Soon....</h2>
+      <h2 className="text-3xl font-bold mt-5 text-center">Coming Soon....</h2>
     </div>
   );
 };
