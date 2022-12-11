01083d9001581f0b6699c0a96640119c2473bd36
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-12-12 00:22:52 +0600

    show all four image

--- src/Pages/Projects/ProjectDetails.js
+++ src/Pages/Projects/ProjectDetails.js
@@ -5,18 +5,23 @@ import { useLoaderData } from 'react-router-dom';
 const ProjectDetails = () => {
   const project = useLoaderData();
 
-  const { name, img, live, github, description } = project;
+  const { name, live, github, description, images } = project;
   const projectDescription = description;
 
   return (
     <div className='container mx-auto my-16'>
       <div className='max-w-6xl mx-auto'>
         <div className="card shadow-xl border border-slate-600 h-full m-3 lg:m-0">
-          <figure className='w-full'>
-            <PhotoView src={img}>
-              <img className='w-full h-full' src={img} alt={name} />
-            </PhotoView>
-          </figure>
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-6 p-5'>
+            {
+              images.map((image, index) => <figure key={index} className='w-full cursor-pointer'>
+                <PhotoView src={image}>
+                  <img className='w-full h-full rounded-md border dark:border-none' src={image} alt={name} />
+                </PhotoView>
+              </figure>
+              )
+            }
+          </div>
           <div className="card-body">
             <h2 className="card-title">{name}</h2>
             <ul className='text-lg list-disc ml-4 grid'>
