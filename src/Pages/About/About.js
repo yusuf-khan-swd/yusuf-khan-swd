c9e2dc65adb52d6471ebe51394b73e55cd922bad
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-12-12 16:41:08 +0600

    modified about section with some grid and style

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -4,27 +4,31 @@ import myPic from "../../assets/my-pic-2.jpg";
 
 const About = () => {
   return (
-    <div className="container mx-auto my-16">
-      <div className="grid grid-cols-1 md:grid-cols-2 m-2">
-        <div>
+    <div className="container mx-auto mt-8 p-4">
+      <div className="grid grid-cols-1 md:grid-cols-5 m-2 gap-8 max-w-5xl mx-auto">
+        <div className="md:col-span-2">
           <PhotoView src={myPic}>
             <img
               src={myPic}
               alt="Yusuf"
               className="h-[450px] border-8 border-slate-400 rounded-md mx-auto md:ml-1 cursor-pointer"
             />
-
           </PhotoView>
         </div>
-        <div className="max-w-md flex justify-center items-center mt-4">
+        <div className="mt-4 md:mt-0 md:col-span-3 flex items-center">
           <div>
             <div>
-              <h2 className="text-3xl font-bold mb-3">About Me</h2>
-              <p className="text-xl leading-relaxed">
-                Hi! This is Yusuf, A MERN stack web developer trying to gain
+              <h2 className="text-4xl font-bold mb-5">About Me</h2>
+              <p className="text-lg leading-relaxed mb-3">
+                Hi ! <br /> This is Yusuf, A MERN stack web developer trying to gain
                 confidence using my potential in the field of “Web Development”.
-                I always focus on learning new technology. I will express my
-                innovative creative skills for self and company growth.
+                I always focus on learning new technology.
+              </p>
+              <p className="text-lg">
+                I wish to work in a dynamic organization that will contribute to
+                my professional and personal growth while I contribute to the
+                growth of the company as well as engage in opportunities to
+                further the company's goals.
               </p>
             </div>
           </div>
