615ca5a9a0d9b1fb587435895f547b4acb396604
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-14 20:35:11 +0600

    add MySkills component

---
+++ src/Pages/Home/MySkills/MySkills.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const MySkills = () => {
+  const skills = [
+    "JavaScript",
+    "TypeScript",
+    "ReactJS",
+    "NextJS",
+    "Redux",
+    "Ant Design",
+    "Material UI",
+    "ExpressJS",
+    "NodeJS",
+    "MongoDB",
+    "Mongoose",
+    "PostgreSQL",
+    "Prisma",
+    "TailwindCSS",
+    "REST APIs",
+    "HTML",
+    "CSS",
+    "Styled Components",
+    "Bootstrap",
+    "React-Router-Dom",
+  ];
+
+  return (
+    <div>
+      <p className="text-2xl font-bold mb-6 text-center uppercase text__shadow">
+        <span className="text-gray-400 font-bold text-3xl">&lt;</span>
+        <span>My Skills</span>
+        <span className="text-gray-400 font-bold">/</span>
+        <span className="text-gray-400 font-bold text-3xl">&gt;</span>
+      </p>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 lg:gap-6">
+        {skills.map((skill, index) => (
+          <button
+            // className="bg-gray-600 text-white m-1 px-2 rounded-md text-sm"
+            className="bg-gray-600 bg-opacity-15 px-4 py-3 rounded-lg shadow text-center flex justify-center items-center gap-2"
+            key={index}
+          >
+            {skill}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MySkills;
