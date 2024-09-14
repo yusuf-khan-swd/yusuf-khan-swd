e9aabbea74724775a675de958e00c7409c972d8f
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-14 20:35:31 +0600

    Display MySkills component in home page

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Projects from "../../Projects/Projects";
 import Banner from "../Banner/Banner";
+import MySkills from "../MySkills/MySkills";
 
 const Home = () => {
   return (
     <div className="container mx-auto">
       <Banner />
+      <MySkills />
       <Projects />
     </div>
   );
