07abd0c9c89c22dd38894aa9b6b476422ea99ee8
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-23 16:17:47 +0600

    add footer component

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  const year = new Date().getFullYear();
+  return (
+    <div>
+      <p className="py-4 text-center m-2">
+        &copy; {year} by{" "}
+        <a href="https://github.com/yusuf-khan-swd" target="_blank">
+          Yusuf Khan
+        </a>{" "}
+        . All Right Reserve
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
