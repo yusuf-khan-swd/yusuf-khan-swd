fbe09ef5c113d57c4946ea398de8f7a22861937b
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-23 17:03:45 +0600

    [done] contact component

--- src/Pages/Others/Contact/Contact.js
+++ src/Pages/Others/Contact/Contact.js
@@ -1,23 +1,38 @@
 import React from "react";
+import { FaEnvelope, FaPhone } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
 
 const Contact = () => {
   return (
-    <div>
-      <div>
-        <h3>Contact</h3>
-        <h3>Get in Touch</h3>
+    <div className="my-12 m-2">
+      <div className="text-center  mb-6">
+        <h3 className="text-xl">Contact</h3>
+        <h3 className="text-2xl">Get in Touch</h3>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <div>
-          <p>Phone</p>
-          <p>+880 16290-90158</p>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+        <div className="p-8 text-center border border-gray-600 rounded-lg shadow-lg">
+          <p className="flex justify-center mb-3">
+            <a href="tel:8801629090158">
+              <FaPhone className="text-slate-500 hover:text-slate-600 text-5xl"></FaPhone>
+            </a>
+          </p>
+          <p className="mb-2 font-semibold">Phone</p>
+          <p>+880 16290 90158</p>
         </div>
-        <div>
-          <p>Email</p>
+        <div className="p-8 text-center border border-gray-600 rounded-lg shadow-lg">
+          <p className="flex justify-center mb-3">
+            <a href="mailto:yusufkhan.swd@gmail.com">
+              <FaEnvelope className="text-slate-500 hover:text-slate-600 text-5xl"></FaEnvelope>
+            </a>
+          </p>
+          <p className="mb-2 font-semibold">Email</p>
           <p>yusufkhan.swd@gamil.com</p>
         </div>
-        <div>
-          <p>Address</p>
+        <div className="p-8 text-center border border-gray-600 rounded-lg shadow-lg">
+          <p className="flex justify-center mb-3">
+            <FaLocationDot className="text-slate-500 hover:text-slate-600 text-5xl"></FaLocationDot>
+          </p>
+          <p className="mb-2 font-semibold">Address</p>
           <p>Chittagong, Bangladesh</p>
         </div>
       </div>
