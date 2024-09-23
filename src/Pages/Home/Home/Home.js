8e68b9d6f8f9b20b2da6a3b7d6b1b1d7fa1fe085
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-23 18:16:45 +0600

    render MySkills and MyEducation component based on state

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Projects from "../../Projects/Projects";
 import Banner from "../Banner/Banner";
 import Contact from "../Contact/Contact";
 import MyEducation from "../MyEducation/MyEducation";
 import MySkills from "../MySkills/MySkills";
 
+const CONTENT = { skills: "skills", education: "education" };
+
 const Home = () => {
+  const [showContent, setShowContent] = useState(CONTENT.skills);
+
   return (
     <div className="container mx-auto">
       <Banner />
       <div className="text-center mb-6">
-        <button className="btn btn-ghost m-1">Skills</button>
-        <button className="btn btn-ghost m-1">Education</button>
+        <button
+          className="btn btn-ghost m-1"
+          onClick={() => setShowContent(CONTENT.skills)}
+        >
+          Skills
+        </button>
+        <button
+          className="btn btn-ghost m-1"
+          onClick={() => setShowContent(CONTENT.education)}
+        >
+          Education
+        </button>
       </div>
-      <MyEducation />
-      <MySkills />
+      {showContent === CONTENT.skills ? (
+        <MySkills />
+      ) : showContent === CONTENT.education ? (
+        <MyEducation />
+      ) : null}
       <Projects />
       <Contact />
     </div>
