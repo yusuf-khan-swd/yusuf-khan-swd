b8b3ac7a2989dc64975eaee03bc119e11d3c0a9e
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-23 18:19:48 +0600

    change button color based on state

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -15,13 +15,17 @@ const Home = () => {
       <Banner />
       <div className="text-center mb-6">
         <button
-          className="btn btn-ghost m-1"
+          className={`btn btn-ghost m-1 ${
+            showContent === CONTENT.skills && "text-gray-300"
+          }`}
           onClick={() => setShowContent(CONTENT.skills)}
         >
           Skills
         </button>
         <button
-          className="btn btn-ghost m-1"
+          className={`btn btn-ghost m-1 ${
+            showContent === CONTENT.education && "text-gray-300"
+          }`}
           onClick={() => setShowContent(CONTENT.education)}
         >
           Education
