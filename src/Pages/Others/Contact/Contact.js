2d5c522dd9877b3377b8f6c76cc92d26b2afb728
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-23 16:32:42 +0600

    add contact form

---
+++ src/Pages/Others/Contact/Contact.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <div>
+        <h3>Contact</h3>
+        <h3>Get in Touch</h3>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <div>
+          <p>Phone</p>
+          <p>+880 16290-90158</p>
+        </div>
+        <div>
+          <p>Email</p>
+          <p>yusufkhan.swd@gamil.com</p>
+        </div>
+        <div>
+          <p>Address</p>
+          <p>Chittagong, Bangladesh</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
+<h3>Contact</h3>;
